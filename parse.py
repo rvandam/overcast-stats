d6ea7ebcab30b8a40c631fb51fc012c8f1efb810
Rob Van Dam <rvandam00@gmail.com>
2021-08-02 21:09:18 -0600

    Init

---
+++ parse.py
@@ -0,0 +1,36 @@
+import xml.etree.ElementTree as ET
+from pprint import pprint
+from collections import defaultdict
+
+def parseXML(xmlfile):
+
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+    counts = {}
+    for item in root.findall('./body/outline/outline[@type="rss"]'):
+        print(item.attrib['text'])
+        stats = defaultdict(int)
+        if (len(item) > 0):
+            for episode in item.findall('outline[@type="podcast-episode"]'):
+                stats['total'] += 1
+                for attr in episode.attrib:
+                    try:
+                        stats[attr] += int(episode.attrib[attr])
+                    except Exception:
+                        pass
+        print(stats)
+#                print(item[0].tag, item[0].attrib)
+#        print(item.tag, item.attrib)
+#    for child in root:
+#        print(child.tag, child.attrib)
+#        for gchild in child:
+#            print("\t", gchild.tag, gchild.attrib)
+#            for ggchild in gchild:
+#                print("\t\t", ggchild.tag, ggchild.attrib)
+
+
+def main():
+    parseXML('overcast.opml')
+
+if __name__ == "__main__":
+    main()
